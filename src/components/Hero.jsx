c8c84669431db6ae82a9b675e5d7390c1f282008
mohdfurqan51 <mohdfurqan23789@gmail.com>
2023-05-20 01:54:04 +0530

    changed title of hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-80 mx-auto`}>
+    <section className={`relative w-full h-80 lg:h-screen mx-auto`}>
       <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex
       flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
@@ -16,7 +16,7 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Furqan</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden'/> interfaces and web applications.
+            I develop user interfaces<br className='sm:block hidden'/> and web applications using React.
           </p>
         </div>
 
